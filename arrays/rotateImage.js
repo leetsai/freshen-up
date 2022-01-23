4c3ce5931ebb955da8cea2c60455b02efc579c45
leetsai <lee.x.tsai@gmail.com>
2022-01-23 16:05:32 -0500

    add pro tips to rotateImage problems

--- arrays/rotateImage.js
+++ arrays/rotateImage.js
@@ -6,6 +6,7 @@ const rotateImage = function(matrix) {
   for (let i = 0; i < len; i++) {
     for (let j = i; j < matrix[i].length; j++) {
       let temp = matrix[i][j];
+
       matrix[i][j] = matrix[j][i];
       matrix[j][i] = temp;
     }
@@ -15,6 +16,7 @@ const rotateImage = function(matrix) {
   for (let i = 0; i < len; i++) {
     for (let j = 0; j < len / 2; j++) {
       let temp = matrix[i][j];
+      
       matrix[i][j] = matrix[i][len - 1 - j];
       matrix[i][len - 1 - j] = temp;
     }
--- arrays/rotateImage_rotateInPlace.js
+++ arrays/rotateImage_rotateInPlace.js
@@ -4,10 +4,20 @@ const rotateImage = function(matrix) {
 
   for (let i = 0; i < Math.floor(len / 2); i++) {
     for (let j = 0; j < Math.floor(len / 2) + len % 2; j++) {
+      // storing bottom left because we're going counter-clockwise for clockwise 90 degree rotation
       let temp = matrix[len - 1 - j][i];
+
+      // tip for remembering i's and j's: if the row or column stays in place, give it the outter loop
+      //   the converse is true: if a row or column changes, give it the inner loop
+      // if starting on the right, then len - 1 - (i or j)
+      // if starting on the bottom, then len - 1 - (i or j)
+      // bottom left
       matrix[len - 1 - j][i] = matrix[len - 1 - i][len - 1 - j];
+      // bottom right
       matrix[len - 1 - i][len - 1 - j] = matrix[j][len - 1 - i];
+      // top right
       matrix[j][len - 1 - i] = matrix[i][j];
+      // top left
       matrix[i][j] = temp;
     }
   }
