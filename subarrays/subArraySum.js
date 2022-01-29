556ddb435c08ea6354c93039a75bfe0d9869359a
leetsai <lee.x.tsai@gmail.com>
2022-01-29 00:10:17 -0500

    add alternative logic and some logs

--- subarrays/subArraySum.js
+++ subarrays/subArraySum.js
@@ -13,3 +13,22 @@ const maxSubArray = (nums) => {
   
   return max;
 };
+
+const maxSubArrayAlternative = (nums) => {
+  let max = nums[0];
+  let curr = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    let n = nums[i];
+
+    curr = Math.max(n, curr + n);
+    max = Math.max(max, curr);
+  }
+
+  return max;
+}
+
+const nums = [-2,1,-3,4,-1,2,1,-5,4];
+
+console.log(maxSubArray(nums));
+console.log(maxSubArrayAlternative(nums));
