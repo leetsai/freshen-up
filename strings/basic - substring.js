44aaa0bfb7891b8982b4831bf546c1b782807408
leetsai <lee.x.tsai@gmail.com>
2022-01-29 23:37:18 -0500

    practice substring method

---
+++ strings/basic - substring.js
@@ -0,0 +1,22 @@
+const nums = "0123456789";
+
+console.log(nums.substring(0));
+// 0123456789
+
+console.log(nums.substring(0,0));
+// ""
+
+console.log(nums.substring(0,1));
+// 0
+
+console.log(nums.substring(0,10));
+// 0123456789
+
+console.log(nums.substring(0,9));
+// 012345678
+
+console.log(nums.substring(9));
+// 9
+
+console.log(nums.substring(10));
+// ""
