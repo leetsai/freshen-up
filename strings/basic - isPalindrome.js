b4c2f6232b0271a92d2ca2edf57bea7aee650bbe
leetsai <lee.x.tsai@gmail.com>
2022-01-30 19:20:26 -0500

    preprocess using regex and minding cases

--- strings/basic - isPalindrome.js
+++ strings/basic - isPalindrome.js
@@ -22,3 +22,22 @@ console.log(isPalindrome(a));
 console.log(isPalindrome(aba));
 console.log(isPalindrome(emptyString));
 console.log(isPalindrome(cabba));
+
+const lcIsPalindrome = s => {
+  simplifiedS = s.replace(/[^a-zA-Z0-9]/g, "");
+
+  let i = 0, j = simplifiedS.length - 1;
+
+  while (i < j) {
+    if (simplifiedS[i].toLowerCase() !== simplifiedS[j].toLowerCase()) return false;
+
+    i++, j--;
+  }
+
+  return true;
+}
+
+console.log(lcIsPalindrome(a));
+console.log(lcIsPalindrome(aba));
+console.log(lcIsPalindrome(emptyString));
+console.log(lcIsPalindrome(cabba));
