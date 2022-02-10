0a899c57ed64800c676fd680b9374c9ee237a176
leetsai <lee.x.tsai@gmail.com>
2022-02-09 21:53:01 -0500

    add alternative implementation to slow and fast pointer solution

--- patterns/fastAndSlowPointers/easy - hasCycle.js
+++ patterns/fastAndSlowPointers/easy - hasCycle.js
@@ -14,3 +14,19 @@ const hasCycle = head => {
 
   return true;
 }
+
+const hasCycleAlternative = head => {
+  if (head === null) return false;
+
+  // different nodes so our slow === head condition isn't immediately met
+  let slow = head, fast = head.next;
+
+  while (fast !== null && fast.next !== null) {
+    if (slow === fast) return true;
+
+    slow = slow.next;
+    fast = fast.next.next
+  }
+
+  return false;
+}
