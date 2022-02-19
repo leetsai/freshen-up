0e57d15222d87f95ffb1da27f7213784b9d98bc2
leetsai <lee.x.tsai@gmail.com>
2022-02-19 01:18:55 -0500

    solve using && but would not work if the problem changes

---
+++ practiceQuestions/deleteOneForStrictlyIncreasing.js
@@ -0,0 +1,27 @@
+/*
+  Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+
+  Note: sequence a0, a1, ..., an is considered to be a strictly increasing if a0 < a1 < ... < an. Sequence containing only one element is also considered to be strictly increasing.
+*/
+
+function solution(sequence) {
+  let quota = 1;
+
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] <= sequence[i - 1]) {
+      quota--;
+      // check if more than one nonincreasing found
+      if (quota <= 0) return false;
+
+      // if this condition fails, we know deleting current or next won't help
+      if (
+        sequence[i] <= sequence[i - 2] &&
+        sequence[i + 1] <= sequence[i - 1]
+      ) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
