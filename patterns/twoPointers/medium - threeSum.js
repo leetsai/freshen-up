49c8308cfae8b48899d44ab5cebe32f224e9a4d4
leetsai <lee.x.tsai@gmail.com>
2022-03-01 10:25:00 -0500

    add clarifying comments to aid understanding

--- patterns/twoPointers/medium - threeSum.js
+++ patterns/twoPointers/medium - threeSum.js
@@ -1,9 +1,20 @@
-const threeSum = nums => {
-  const sorted = nums.sort((a,b) => a - b);
+/*
+  Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+  Notice that the solution set must not contain duplicate triplets.
+
+  Input: nums = [-1,0,1,2,-1,-4]
+  Output: [[-1,-1,2],[-1,0,1]]
+*/
+
+const threeSum = (nums) => {
+  const sorted = nums.sort((a, b) => a - b);
   const ans = [];
 
   for (let i = 0; i < sorted.length; i++) {
+    // if first sorted element is > 0, then it's not possible to get to sum of 0
     if (sorted[0] > 0) break;
+    // we want only unique triplets
     if (sorted[i] === sorted[i - 1]) continue;
 
     let lo = i + 1;
@@ -20,8 +31,9 @@ const threeSum = nums => {
       } else {
         ans.push([sorted[i], sorted[lo], sorted[hi]]);
         lo++;
-        hi--
-        
+        hi--;
+
+        // again, we want unique triplets. checking for uniqueness at i, lo (i + 1) guarantees uniqueness at hi
         while (sorted[lo] === sorted[lo - 1]) {
           lo++;
         }
@@ -32,8 +44,8 @@ const threeSum = nums => {
   return ans;
 };
 
-const sample = [-1,0,1,2,-1,-4];
-const sample2 = [-1,0,1,2,-1,-4,-2,-3,3,0,4];
+const sample = [-1, 0, 1, 2, -1, -4];
+const sample2 = [-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4];
 
 console.log(threeSum(sample));
 // [[-1,-1,2],[-1,0,1]]
